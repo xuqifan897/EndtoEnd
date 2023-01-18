733a6a945f0d59b8bee9136b202b31f46daf81c7
Qifan Xu <xuqifan897@gmail.com>
2023-01-17 17:14:43 -0800

    the work of Jan 17

--- scripts/UCLAPatientsNew.py
+++ scripts/UCLAPatientsNew.py
@@ -15,10 +15,10 @@ of CT and MR differred a lot. So this time, we selected another set of patients.
 globalFolder = '/data/datasets/UCLAPatients'
 rawFolder = '/data/datasets/UCLAPatients/rawDataNew'
 anonymousFolder = os.path.join(globalFolder, 'anonymousDataNew')
-visuallize = os.path.join(globalFolder, 'visualizeNew')
+visFolder = os.path.join(globalFolder, 'visNew')
 patients = os.listdir(rawFolder)
 patientTree = []
-numPatients = 8
+numPatients = 7
 
 # list all attributes to wipe out in CT
 AttrWipeCT = ['AccessionNumber', 'AcquisitionDate', 'AcquisitionNumber', 'AcquisitionTime', 'ContentDate',
@@ -48,6 +48,37 @@ AttrWipeDose = ['AccessionNumber', 'AcquisitionDate', 'AcquisitionTime', 'Conten
     'InstanceCreationTime', 'InstitutionName', 'Laterality', 'Manufacturer', 'ManufacturerModelName', 'OperatorsName', 
     'PatientAge', 'PatientBirthDate', 'PatientID', 'PatientName', 'PatientSex', 'ReferringPhysicianName', 'SeriesDate', 
     'SeriesDescription', 'SeriesTime', 'StudyDate', 'StudyDescription', 'StudyTime', 'parent']
+
+
+anatomies = [
+    ['Skin', 'GTV', 'PTV 50 uncropped', 'O_Cord', 'O_Esgs', 'O_Hrt', 'O_Livr', 'O_Stmc', 'O_Kdny_Rt', 
+    'O_Kdny_Lt', 'O_Bwel_Sm', 'O_Bwel_Lg', 'O_Duodenum', 'PTV_CROP', 'PTV_LOW', 'PTV_HIGH'],
+
+    ['Skin', 'O_Cord', 'O_Esgs', 'O_Hrt', 'O_Livr', 'O_Stmc', 'O_Kdny_Rt', 'O_Kdny_Lt', 'O_Bwel_Sm', 
+    'O_Bwel_Lg', 'GTV ACR', 'PTV ACR uncroppe', 'PTV_CROPPED', 'PTV_LOW', 'PTV_HIGH'],
+
+    ['Skin', 'O_Cord', 'O_Esgs', 'O_Hrt', 'O_Livr', 'O_Stmc', 'O_Kdny_Rt', 'O_Kdny_Lt', 'O_Bwel', 
+    'O_Duod', 'GTV', 'PTV uncropped', 'PTV_CROPPED', 'PTV_LOW', 'PTV_HIGH'],
+
+    ['Skin', 'O_Cord', 'O_Hrt', 'O_Livr', 'O_Stmc', 'O_Kdny_Rt', 'O_Bwel', 'O_Vssl', 'GTV', 
+    'PTV uncropped', 'O_DUOD', 'PTV_CROPPED', 'PTV_HIGH', 'PTV_LOW'],
+
+    ['Skin', 'O_Cord', 'O_Lung_Lt', 'O_Lung_Rt', 'O_Hrt', 'O_Livr', 'O_Lung_Tt', 'O_Kdny_Rt', 'O_Kdny_Lt', 
+    'O_Vssl', 'O_Bwl', 'O_Duod', 'O_Spln', 'PTV 3mm uncroppe', 'O_Stmch', 'PTV_HIGH', 'PTV_LOW', 'PTV_CROPPED'],
+
+    ['O_Bldr', 'O_Femr_Lt', 'O_Femr_Rt', 'O_Rctm', 'Skin', 'O_Cord', 'O_Spln', 'O_Livr', 'O_Stmc', 
+    'O_Kdny_Rt', 'O_Kdny_Lt', 'O_Bwel', 'O_Duod', 'O_Vssl', 'GTV', 'PTV 50 uncropped', 'PTV_CROPPED', 
+    'PTV_HIGH', 'PTV_LOW'],
+
+    ['Skin', 'GTV', 'PTV 50_3mm', 'PTV_High', 'O_Duod', 'O_Stmc', 'O_Bwel_Sm', 'O_Bwel_Lg', 'O_Cord', 
+    'O_Esgs', 'O_Vessle', 'O_Kdny_Rt', 'O_Kdny_Lt', 'O_Livr', 'O_Skin', 'PTV_crop', 'O_Nrlms', 'O_Nrmls_3mm']
+]
+
+
+colors = [(231, 203, 107), (152, 153, 139), (97, 111, 175), (60, 42, 162), (75, 172, 12), 
+    (35, 69, 171), (86, 201, 174), (16, 103, 52), (204, 19, 136), (121, 104, 80), 
+    (96, 32, 200), (57, 131, 240), (17, 97, 12), (255, 218, 243), (245, 242, 167), 
+    (94, 100, 206), (238, 227, 161), (202, 254, 156), (76, 41, 217), (158, 239, 248)]
 
 
 def folderEvaluate(template):
@@ -314,8 +345,176 @@ def visCTMR():
     Firstly, convert the DICOM images of CT and MR to PNG images for 
     visualization
     """
+    if not os.path.isdir(visFolder):
+        os.mkdir(visFolder)
+    scale = 255
+    modality = 'CT'
+
+    # save MR images
+    if modality == 'MR':
+        for i in range(numPatients):
+            idx = i + 1
+            patientName = 'patient{}'.format(idx)
+            patFolder = os.path.join(anonymousFolder, patientName)
+            MRFolder = os.path.join(patFolder, 'MR')
+            MRoutput = os.path.join(visFolder, patientName, 'MR')
+            if not os.path.isdir(MRoutput):
+                os.makedirs(MRoutput)
+            files = os.listdir(MRFolder)
+            files.sort()
+            for file in files:
+                file_ = os.path.join(MRFolder, file)
+                pixel_array = pydicom.dcmread(file_).pixel_array
+                roof = np.max(pixel_array)
+                pixel_array = np.uint8(pixel_array / roof * scale)
+                outFile = file.split('.')[0]
+                outFile = os.path.join(MRoutput, outFile + '.png')
+                plt.imsave(outFile, pixel_array, cmap='gray')
+            print(patientName)
+    elif modality == 'CT':
+        for i in range(numPatients):
+            idx = i + 1
+            patientName = 'patient{}'.format(idx)
+            patFolder = os.path.join(anonymousFolder, patientName)
+            CTFolder = os.path.join(patFolder, 'CT')
+            CToutput = os.path.join(visFolder, patientName, 'CT')
+            if not os.path.isdir(CToutput):
+                os.makedirs(CToutput)
+            files = os.listdir(CTFolder)
+            files.sort()
+            for file in files:
+                file_ = os.path.join(CTFolder, file)
+                pixel_array = pydicom.dcmread(file_).pixel_array
+                roof = np.max(pixel_array)
+                pixel_array = np.uint8(pixel_array / roof * scale)
+                outFile = file.split('.')[0]
+                outFile = os.path.join(CToutput, outFile + '.png')
+                plt.imsave(outFile, pixel_array, cmap='gray')
+            print(patientName)
+
+
+def showAnatomy():
+    # task = 'sanity'
+    task = 'MR'
+
+    # Firstly, check that the selected anatomies exist
+    if task == 'sanity':
+        for i in range(numPatients):
+            idx = i + 1
+            patientName = 'patient{}'.format(idx)
+            patFolder = os.path.join(anonymousFolder, patientName)
+            MRrtPath = os.path.join(patFolder, 'MRrt.dcm')
+            MRFolder = os.path.join(patFolder, 'MR')
+            MRstruct = RTStructBuilder.create_from(dicom_series_path=MRFolder, 
+                rt_struct_path=MRrtPath)
+            MRROInames = MRstruct.get_roi_names()
+
+            CTrtPath = os.path.join(patFolder, 'CTrt.dcm')
+            CTFolder = os.path.join(patFolder, 'CT')
+            CTstruct = RTStructBuilder.create_from(dicom_series_path=CTFolder, 
+                rt_struct_path=CTrtPath)
+            CTROInames = CTstruct.get_roi_names()
+
+            ana = anatomies[i]
+            flag = True
+            for name in ana:
+                if name not in MRROInames:
+                    flag = False
+                    break
+            print('{} MR {}'.format(patientName, 'correct' if flag else 'incorrect'))
+            flag = True
+            for name in ana:
+                if name not in CTROInames:
+                    flag = False
+                    break
+            print('{} CT {}\n'.format(patientName, 'correct' if flag else 'incorrect'))
+    
+    # Secondly, draw contours
+    # for i in range(numPatients):
+    i = 4
+    patientName = 'patient{}'.format(i+1)
+    patFolder = os.path.join(anonymousFolder, patientName)
+    dicomFolder = os.path.join(patFolder, task)
+    RTfile = os.path.join(patFolder, task + 'rt.dcm')
+    RT = RTStructBuilder.create_from(dicom_series_path=dicomFolder, rt_struct_path=RTfile)
+    anatomyList = anatomies[i]
+    result = drawContours(dicomFolder, RT, anatomyList)
+    outFolder = os.path.join(visFolder, patientName, task+'contour')
+    writeResult(outFolder, result)
+    print(patientName)
+
+
+def writeResult(outFolder, result):
+    if not os.path.isdir(outFolder):
+        os.mkdir(outFolder)
+    nSlices = result.shape[3]
+    for i in range(nSlices):
+        fileName = '{:03d}.png'.format(i+1)
+        fileName = os.path.join(outFolder, fileName)
+        plt.imsave(fileName, result[:, :, :, i])
+        
+
+def drawContours(dicomFolder, RT, anatomyList):
+    """
+    This function draws contour on the dicom images
+    """
+    # firstly, get the shape of the images
+    exampleMask = RT.get_roi_mask_by_name(anatomyList[0])
+    shape = exampleMask.shape
+    height, width, slices = shape
+    channels = 3
+    result = np.zeros((height, width, channels, slices), dtype=np.uint8)
+
+    # # take a look
+    # print(np.max(exampleMask))
+    # plt.imshow(exampleMask[:, :, 40])
+    # plt.show()
+    # exit()
+
+    # get slices first
+    scale = 255
+    for i in range(slices):
+        slicePath = os.path.join(dicomFolder, '{:03d}.dcm'.format(i+1))
+        pixel_array = pydicom.dcmread(slicePath).pixel_array
+        roof = np.max(pixel_array)
+        pixel_array = np.uint8(pixel_array / roof * scale)
+        pixel_array = np.stack((pixel_array, ) * channels, axis=2)
+        result[:, :, :, i] = pixel_array
+    
+    image = np.ones((height, width, channels), dtype=np.uint8) * scale
+    for j, anatomy in enumerate(anatomyList):
+        mask = RT.get_roi_mask_by_name(anatomy)
+        mask = np.flip(mask, axis=2)
+        for i in range(slices):
+            maskSlice = mask[:, :, i]
+            maskSlice = np.uint8(maskSlice * scale)
+            contours, _ = cv2.findContours(maskSlice, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            image[:] = result[:, :, :, i]
+            result[:, :, :, i] = cv2.drawContours(image, contours, -1, colors[j], 1)
+    return result
+
+
+def colorGen():
+    """
+    this is to generate 20 different colors
+    """
+    colors = []
+    Ncolors = 20
+    norm = 256
+    for i in range(Ncolors):
+        b = int(np.random.rand() * norm)
+        g = int(np.random.rand() * norm)
+        r = int(np.random.rand() * norm)
+        color = (b, g, r)
+        colors.append(color)
+    
+    for anatomy in anatomies:
+        print(len(anatomy))
 
 
 
 if __name__ == '__main__':
-    anonymize()
+    # anonymize()
+    # visCTMR()
+    showAnatomy()
+    # colorGen()
